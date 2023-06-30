9e989df68bcef823090434a1ded08daf9cbbf72d
ThiLinhares <thiagozudo@hotmail.com>
2023-06-30 14:50:55 -0300

    Configure cypress grep

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,8 +1,8 @@
 /* eslint-disable indent */
-/* eslint-disable no-undef */
+import registerCypressGrep from '@cypress/grep/src/support'
+registerCypressGrep()
 
 import './commands'
-
 
 import 'cypress-mailosaur'
 
