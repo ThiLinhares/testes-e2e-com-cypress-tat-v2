04edf5fd332e70474a7c6f0fe21bab42998992da
ThiLinhares <thiagozudo@hotmail.com>
2023-06-30 14:31:33 -0300

    Configure cypress cloud

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,12 +2,7 @@
 /* eslint-disable no-undef */
 
 import './commands'
-// eslint-disable-next-line no-unused-vars
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-    })
+
 
 import 'cypress-mailosaur'
 
